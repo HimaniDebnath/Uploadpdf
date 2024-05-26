30bb2bff3c376bef1f2ec68b0a8dce30818a37b4
Nehakumari123123 <techieneha.it@gmail.com>
2024-05-26 23:17:18 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [file, setFile] = useState(null);
+
+  const [pdf, setpdf] = useState("");
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+    console.log(event.target.files[0].name);
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      try {
+        const endpoint = "http://127.0.0.1:8000/upload/";
+        const response = await fetch(endpoint, {
+          method: "POST",
+          body: formData,
+        });
+
+        if (response.ok) {
+          console.log("file uploaded Successfully");
+        } else {
+          console.log("FIle upload unsuccessful");
+        }
+      } catch (error) {
+        setpdf("PDF FIle");
+      }
+    }
+    console.log("i am himani");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>Upload pdf</h1>
+        <input type="file" onChange={handleFileChange} />
+
+        <button className="Secbtn" type="submit">
+          Submit
+        </button>
+      </form>
+      <textarea name="textarea" cols="30" rows="10" readOnly value={pdf}>
+        {pdf}
+      </textarea>
+    </>
   );
 }
 
